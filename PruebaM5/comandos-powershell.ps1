542fb86dd64bf1ee35c98a7d57e5df89224dc607
german-rs <ing.germanriveros@gmail.com>
2025-07-17 15:30:39 -0400

    ítem - asegurar superficie de ataque

--- PruebaM5/comandos-powershell.ps1
+++ PruebaM5/comandos-powershell.ps1
@@ -1,4 +1,6 @@
-# Configuraciones iniciales.
+#==========================
+# CONFIGURACIONES INICIALES 
+#==========================
 
 # 1.  nombre del servidor
 Rename-Computer -NewName "SERV02" -Force
@@ -27,28 +29,33 @@ New-NetFirewallRule -Name "Allow-Ping" `
   -Enabled True `
   -Profile Any
 
-# 5. Habilitar PowerShell Remoting
+#============================
+# CONFIGURACIÓN DE POWERSHELL 
+#     REMOTING SEGURO
+#============================
+
+# 1. Habilitar PowerShell Remoting
 Enable-PSRemoting -Force
 
-# 5.1. Verifica que WinRM está en ejecución
+# 1.1. Verifica que WinRM está en ejecución
 Get-Service WinRM
 
-# 5.2. Verifica que existe el "listener" HTTP
+# 1.2. Verifica que existe el "listener" HTTP
 winrm enumerate winrm/config/listener
 
-# 6. Agrega el usuario de dominio al grupo local "Administradores"
+# 2. Agrega el usuario de dominio al grupo local "Administradores"
 
 Add-LocalGroupMember -Group "Administradores" -Member "greenapple\griveros"
 
-# 6.1 Se verifica.
+# 2.1 Se verifica.
 Get-LocalGroupMember -Group "Administradores"
 
-# 7. Se verifica desde servidor principal DC01 
+# 2.2 Se verifica desde servidor principal DC01 
 Enter-PSSession -ComputerName SERV02 -Credential (Get-Credential)
 
 #============================================
 # RESTRINGIR EL ACCESO A USUARIOS ESPECÍFICOS 
-# MEDIANTE POLÍTICAS DE SEGURIDAD
+#     MEDIANTE POLÍTICAS DE SEGURIDAD
 #============================================
 
 # 1. Verificar si existe el grupo “Remote Management Users” en SERV02  
@@ -63,6 +70,44 @@ Set-PSSessionConfiguration -Name Microsoft.PowerShell -ShowSecurityDescriptorUI
 # 3.2 Reiniciar el servicio WinRM. 
 Restart-Service WinRM
 
+
+#==============================
+# CONFIGURAR SERVICIOS CRÍTICOS
+#             DHCP
+#==============================
+
+# 1.  Instalar el rol DHCP
+Install-WindowsFeature -Name 'DHCP' -IncludeManagementTools
+
+# 2.  Verificación
+Get-WindowsFeature -Name DHCP
+
+#3. Cambiar a usuario administrador de dominio
+runas /user:greenapple\administrator powershell
+
+# 4. Autorizar el servidor DHCP en Active Directory.
+Add-DhcpServerInDC -DnsName "serv02.greenapple.local" -IPAddress 192.168.2.4
+
+# 5.  Verificación
+Get-DhcpServerInDC
+
+# 6. Crear ámbito DHCP.
+Add-DhcpServerv4Scope `
+  -Name "RedLocal" `
+  -StartRange 192.168.2.100 `
+  -EndRange 192.168.2.200 `
+  -SubnetMask 255.255.255.0 `
+  -State Active
+
+# 7. Verificación del ámbito DHCP
+Get-DhcpServerv4Scope
+
+# 8. Configuración la puerta de enlace (Gateway) y servidor DNS.
+Set-DhcpServerv4OptionValue -ScopeId 192.168.2.0 -Router 192.168.2.1
+
+Set-DhcpServerv4OptionValue -ScopeId 192.168.2.0 -DnsServer 192.168.2.2 -DnsDomain greenapple.local
+
+# 9. Asegurar la superficie de ataque.
 
 
 
